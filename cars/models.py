dbabc7a4d593904f99379824861e36f0e5d01edd
errorLAD <ab.mishra@yahoo.com>
2022-09-16 23:17:33 +0530

    first commit

---
+++ cars/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from datetime import datetime
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
+
+# Create your models here.
+class Car(models.Model):
+
+    state_choice =  (
+    ("Andhra Pradesh","Andhra Pradesh"),
+    ("Arunachal Pradesh ","Arunachal Pradesh "),
+    ("Assam","Assam"),
+    ("Bihar","Bihar"),
+    ("Chhattisgarh","Chhattisgarh"),
+    ("Goa","Goa"),
+    ("Gujarat","Gujarat"),
+    ("Haryana","Haryana"),
+    ("Himachal Pradesh","Himachal Pradesh"),
+    ("Jammu and Kashmir ","Jammu and Kashmir "),
+    ("Jharkhand","Jharkhand"),
+    ("Karnataka","Karnataka"),
+    ("Kerala","Kerala"),
+    ("Madhya Pradesh","Madhya Pradesh"),
+    ("Maharashtra","Maharashtra"),
+    ("Manipur","Manipur"),
+    ("Meghalaya","Meghalaya"),
+    ("Mizoram","Mizoram"),
+    ("Nagaland","Nagaland"),
+    ("Odisha","Odisha"),
+    ("Punjab","Punjab"),
+    ("Rajasthan","Rajasthan"),
+    ("Sikkim","Sikkim"),
+    ("Tamil Nadu","Tamil Nadu"),
+    ("Telangana","Telangana"),
+    ("Tripura","Tripura"),
+    ("Uttar Pradesh","Uttar Pradesh"),
+    ("Uttarakhand","Uttarakhand"),("West Bengal","West Bengal"),("Andaman and Nicobar Islands","Andaman and Nicobar Islands"),("Chandigarh","Chandigarh"),("Dadra and Nagar Haveli","Dadra and Nagar Haveli"),("Daman and Diu","Daman and Diu"),("Lakshadweep","Lakshadweep"),("National Capital Territory of Delhi","National Capital Territory of Delhi"),
+    ("Puducherry","Puducherry"))
+
+    year_choice = []
+    for r in range(2000, (datetime.now().year+1)):
+        year_choice.append((r,r))
+
+    
+
+    door_choices = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('7', '7'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10'),
+        ('11', '11'),
+        ('12', '12'),
+        ('13', '13'),
+        ('14', '14'),
+        ('15', '15'),
+    )
+
+    car_title = models.CharField(max_length=255)
+    state = models.CharField(choices=state_choice, max_length=100)
+    city = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField(('year'), choices=year_choice)
+    condition = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = RichTextField()
+    car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+   
+  
+    doors = models.CharField(choices=door_choices, max_length=10)
+
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.car_title
